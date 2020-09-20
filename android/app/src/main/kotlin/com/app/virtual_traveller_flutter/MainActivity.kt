2042645b19c59d20c4f7021f9694e8d293aaed49
mzdm <zidek.matt@gmail.com>
2020-09-20 11:17:21 +0200

    Initial commit - Empty app

---
+++ android/app/src/main/kotlin/com/app/virtual_traveller_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.virtual_traveller_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Container(
+        color: Colors.grey,
+      ),
+    );
+  }
+}
