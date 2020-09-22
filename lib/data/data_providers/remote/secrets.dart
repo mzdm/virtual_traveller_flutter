fedad4969b3344bd9765a612d1a3f86e6f8e07a8
mzdm <zidek.matt@gmail.com>
2020-09-23 01:39:30 +0200

    update secrets.dart

---
+++ lib/data/data_providers/remote/secrets.dart
@@ -0,0 +1,18 @@
+/// DON'T PUSH REAL VALUES TO VCS - push instead with 'REPLACE_THIS_WITH_YOUR_KEY'
+///
+/// This file contains secret values like:
+///   - api keys
+///   - api secret keys
+class Secrets {
+  /// Obtain an Amadeus API key from here:
+  /// https://developers.amadeus.com/register
+  static const amadeusApiKey = 'REPLACE_THIS_WITH_YOUR_KEY';
+
+  /// Obtain an Amadeus Secret API key from here:
+  /// https://developers.amadeus.com/register
+  static const amadeusSecretKey = 'REPLACE_THIS_WITH_YOUR_KEY';
+
+  /// Obtain the Amadeus Secret API key from here:
+  /// https://developers.amadeus.com/register
+  static const weatherApiKey = 'REPLACE_THIS_WITH_YOUR_KEY';
+}
