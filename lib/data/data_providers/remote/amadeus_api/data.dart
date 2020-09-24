7e2b1b9ca7d3ed832ad5a2bc8e5373dcf4094c19
mzdm <zidek.matt@gmail.com>
2020-09-25 00:01:10 +0200

    API improvements

--- lib/data/data_providers/remote/amadeus_api/data.dart
+++ lib/data/data_providers/remote/amadeus_api/data.dart
@@ -12,7 +12,7 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
 
   /// Fetches the given network call with query parameters.
   ///
-  /// Check whether [_accessToken] is still valid otherwise
+  /// First checks whether accessToken is still valid otherwise
   /// generate a new one.
   Future<String> _getRawDataFromEndpoint(
     String endpointPath,
@@ -53,14 +53,15 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   /// The API provides a list of commercial airports within a 500km (311mi) radius of a
   /// given point that are ordered by relevance, which considers their distance from
   /// the starting point and their yearly flight traffic. For each airport,
-  /// the API provides the name, 3-letter IATA code, time zone and coordinates.
+  /// the API provides the name, 3-letter [IATA code](http://www.iata.org/publications/Pages/code-search.aspx),
+  /// time zone and coordinates.
   @override
   Future<String> getRawNearestAirport(Location location) async {
     final endpointPath = 'v1/reference-data/locations/airports';
     final queryParams = {
-      'latitude': location.lat, // required
-      'longitude': location.long, // required
-      'page[limit]': 3,
+      'latitude': location.lat, // ---required---
+      'longitude': location.long, // ---required---
+      'page[limit]': 3 // maximum items in one page
     };
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
@@ -78,15 +79,25 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   /// This API forms the first step in the [flight booking flow](https://developers.amadeus.com/get-started/create-a-flight-booking-engine-651).
   /// Once a flight is chosen, confirm the real-time price using [Flight Offers Price](https://developers.amadeus.com/self-service/category/air/api-doc/flight-offers-price)
   /// and book the flight with [Flight Create Orders](https://developers.amadeus.com/self-service/category/air/api-doc/flight-create-orders).
+  ///
+  /// [IATA code](http://www.iata.org/publications/Pages/code-search.aspx)
   @override
   Future<String> getRawFlightOffersSearch() async {
     final endpointPath = 'v2/shopping/flight-offers';
     final queryParams = {
-      'originLocationCode': 'value',
-      'destinationLocationCode': 'value',
-      'departureDate': 'value',
-      'adults': 'value',
-    }..removeWhere((key, value) => key == null || value == null);
+      'originLocationCode': 'SYD', // ---required---; IATA code
+      'destinationLocationCode': 'BKK', // ---required---; IATA code
+      'departureDate': '2017-12-25', // ---required---; ISO 8601 YYYY-MM-DD format, e.g. 2017-12-25
+      'returnDate': '2018-02-28', // format same as departureDate, if null then it is one-way, otherwise round-trip
+      'adults': 1, // ---required---, the number of adult travelers (age 12 or older on date of departure)
+      'children': 0, // the number of child travelers (older than age 2 and younger than age 12 on date of departure)
+      'infants': 0, // the number of infant travelers (whose age is less or equal to 2 on date of departure), infants travel on the lap of an adult traveler, and thus the number of infants must not exceed the number of adults
+      'travelClass': 'ECONOMY', // ECONOMY, PREMIUM_ECONOMY, BUSINESS, FIRST; if no travel class is specified, the search considers any travel class
+      'nonStop': false, // if set to true, the search will find only flights going from the origin to the destination with no stop in between
+      'currencyCode': 'EUR', // the preferred currency for the flight offers, currency is specified in the ISO 4217 format, e.g. EUR for Euro
+      'maxPrice': 500, // maximum price per traveler, by default, no limit is applied, if specified, the value should be a positive number with no decimals
+      'max': 100, // maximum number of flight offers to return, if specified, the value should be greater than or equal to 1
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -97,8 +108,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -109,8 +120,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -121,8 +132,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -134,7 +145,7 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     final endpointPath = 'v1/reference-data/airlines';
     final queryParams = {
       'airlineCodes': 'SWA',
-    };
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -145,8 +156,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -157,8 +168,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -169,8 +180,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -181,8 +192,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -193,8 +204,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -205,8 +216,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
@@ -217,8 +228,8 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
     // TODO
     final endpointPath = 'v1/path';
     final queryParams = {
-      'param1': 'value',
-    };
+      '__': '__',
+    }..removeWhere((_, value) => value == null);
 
     return await _getRawDataFromEndpoint(endpointPath, queryParams);
   }
