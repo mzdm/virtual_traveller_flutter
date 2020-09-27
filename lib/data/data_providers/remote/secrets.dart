1b17cd07f9e91ac621e0619ae51709b523f96370
mzdm <zidek.matt@gmail.com>
2020-09-27 15:23:04 +0200

    Fix build

--- lib/data/data_providers/remote/secrets.dart
+++ lib/data/data_providers/remote/secrets.dart
@@ -1,5 +1,3 @@
-import 'package:virtual_traveller_flutter/data/data_providers/remote/secrets_hidden.dart';
-
 /// DON'T PUSH REAL VALUES TO VCS
 /// Create a new file 'secrets_hidden.dart' in this folder with the API keys or
 /// replace them right there instead but be sure to not push them to VCS.
@@ -11,15 +9,15 @@ class Secrets {
   /// Obtain an Amadeus API key from here:
   ///
   /// [https://developers.amadeus.com/register](https://developers.amadeus.com/register)
-  static const amadeusApiKey = SecretsHidden.amadeusApiKey;
+  static const amadeusApiKey = 'SecretsHidden.amadeusApiKey';
 
   /// Obtain an Amadeus Secret API key from here:
   ///
   /// [https://developers.amadeus.com/register](https://developers.amadeus.com/register)
-  static const amadeusSecretKey = SecretsHidden.amadeusSecretKey;
+  static const amadeusSecretKey = 'SecretsHidden.amadeusSecretKey';
 
   /// Obtain - key from here:
   ///
   /// -
-  static const weatherApiKey = SecretsHidden.weatherApiKey;
+  static const weatherApiKey = 'SecretsHidden.weatherApiKey';
 }
