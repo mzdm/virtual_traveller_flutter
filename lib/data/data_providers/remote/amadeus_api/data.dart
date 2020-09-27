2a1d529acac2eed309e5e1bcf5345656079ca969
mzdm <zidek.matt@gmail.com>
2020-09-27 15:12:36 +0200

    Add a description to Amadeus APIs

--- lib/data/data_providers/remote/amadeus_api/data.dart
+++ lib/data/data_providers/remote/amadeus_api/data.dart
@@ -103,6 +103,13 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/flight-cheapest-date-search/api-reference
+  /// *"What are the cheapest dates to fly from Munich to Sao Paulo?"*
+  ///
+  /// The Flight Cheapest Date Search API finds the cheapest dates to travel from
+  /// one city to another. The API provides list of flight options with dates and prices,
+  /// and allows you to order by price, departure date or duration.
+  /// The API provides a link to [Flight Offers Search](https://developers.amadeus.com/self-service/category/air/api-doc/flight-offers-search)
+  /// to complete the flight search using the chosen dates.
   @override
   Future<String> getRawFlightCheapestDateSearch() async {
     // TODO
@@ -115,9 +122,20 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/flight-offers-price/api-reference
+  /// *"What's the final price for this flight?"*
+  ///
+  /// The price and availability of airfare fluctuate constantly, so it's necessary to
+  /// confirm the real-time price before proceeding to book. The Flight Offers Price API
+  /// confirms the availability and final price (including taxes and fees) of
+  /// flights returned by the Flight Offers Search API. The API also provides pricing for
+  /// ancillary products like additional bags or extra-legroom seats and the details on
+  /// the payment information required to complete the booking. This API forms the second step in
+  /// the [flight booking flow](https://developers.amadeus.com/get-started/create-a-flight-booking-engine-651).
+  /// It is used to confirm the price of flights returned by [Flight Offers Search](https://developers.amadeus.com/self-service/category/air/api-doc/flight-offers-price)
+  /// before booking with [Flight Create Orders](https://developers.amadeus.com/self-service/category/air/api-doc/flight-create-orders).
   @override
   Future<String> getRawFlightOffersPrice() async {
-    // TODO
+    // TODO // maybe remove
     final endpointPath = 'v1/path';
     final queryParams = {
       '__': '__',
@@ -127,6 +145,13 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/airport-and-city-search/api-reference
+  /// *"Which cities and airports begin with Lon..?"*
+  ///
+  /// The Airport & City Search API finds airports and cities that match a specific word or string
+  /// of letters. Using this API, you can automatically suggest airports based on what the
+  /// traveler enters in the search field. The API provides a list of airports/cities ordered by
+  /// yearly passenger volume with the name, 3-letter IATA code, time zone and
+  /// coordinates of each airport.
   @override
   Future<String> getRawAirportCitySearch() async {
     // TODO
@@ -139,6 +164,11 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/airline-code-lookup/api-reference
+  /// *"What is the name of the airline with the given IATA code EK?"*
+  ///
+  /// The Airline Code Lookup API lets you find the name of an airline by
+  /// its IATA or ICAO airline code. You can search for multiple airline names by
+  /// including various airline codes in the same request.
   @override
   Future<String> getRawAirlineCodeLookup() async {
     // TODO
@@ -151,6 +181,16 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/flight-inspiration-search/api-reference
+  /// *"What are the cheapest places to fly from Boston?*
+  ///
+  /// Help travelers discover their next destination by finding the cheapest
+  /// flight destinations from a specific city. The Flight Inspiration Search API provides
+  /// a list of destinations from a given city that is ordered by price and
+  /// can be filtered by departure date or maximum price. The API provides
+  /// a link to [Flight Offers Search](https://developers.amadeus.com/self-service/category/air/api-doc/flight-offers-search)
+  /// to search for flights once a destination is chosen and a link to
+  /// [Flight Cheapest Date Search](https://developers.amadeus.com/self-service/category/air/api-doc/flight-cheapest-date-search)
+  /// to check the cheapest dates to fly.
   @override
   Future<String> getRawFlightInspirationSearch() async {
     // TODO
@@ -163,6 +203,14 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/flight-most-booked-destinations/api-reference
+  /// *"Which destinations are most frequently booked by travelers in New Delhi?"*
+  ///
+  /// Get insight into local booking trends by finding which destinations are most frequently
+  /// booked by travelers in a specific city. The Flight Most Booked Destinations API provides a
+  /// list of the most booked destinations by travelers from a given city, each with a
+  /// flight score (flights to the destination as a percentage of total departures) and
+  /// a traveler score (number of passengers traveling to the destination as a
+  /// percentage of total passenger departures).
   @override
   Future<String> getRawFlightMostBooked() async {
     // TODO
@@ -175,6 +223,14 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/air/api-doc/flight-most-traveled-destinations/api-reference
+  /// *"What are the most visited destinations among travelers in New York City?"*
+  ///
+  /// Get insight into local travel habits by finding which destinations are most visited by
+  /// travelers from a specific city. The Flight Most Traveled Destinations API returns a
+  /// list of the most popular flight destinations among travelers from a
+  /// given city, each with a flight score (flights to the destination as a
+  /// percentage of total departures) and a traveler score
+  /// (number of passengers traveling to the destination as a percentage of total passenger departures).
   @override
   Future<String> getRawFlightMostTravelled() async {
     // TODO
@@ -187,6 +243,14 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/trip/api-doc/travel-recommendations/api-reference
+  /// *"If a traveler has shown interest in Berlin, what other destinations would he/she like?"*
+  ///
+  /// The Travel Recommendations API provides travelers personalized destination recommendations
+  /// based on their location and an input destination, such as a previously
+  /// searched flight destination or city of interest. The API uses Artificial Intelligence
+  /// trained on Amadeus historical flight search data to determine which destinations are
+  /// also popular among travelers with similar profiles, and provides a list of
+  /// recommended destinations with name, IATA code, coordinates and similarity score.
   @override
   Future<String> getRawTravelRecommendation() async {
     // TODO
@@ -199,6 +263,18 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/hotel/api-doc/hotel-search/api-reference
+  /// *"What are the best hotel deals during my trip?"*
+  ///
+  /// The Hotel Search API finds the best deals at over 150,000 hotels worldwide.
+  /// The API provides a list of the cheapest hotels in a given location with
+  /// detailed information on each hotel and the option to filter by
+  /// category, chain, facilities or budget range. Once a hotel is chosen, the API provides the
+  /// available rooms and prices. The API as three endpoints - the first finds the
+  /// cheapest available hotels in a chosen city, the second shows room and amenity details and
+  /// the third fetches the real-time price of the desired room.
+  ///
+  /// You can combine this API with the [Hotel Booking API](https://developers.amadeus.com/self-service/category/hotel/api-doc/hotel-booking)
+  /// to build a complete [hotel booking engine](https://developers.amadeus.com/blog/build-hotel-booking-engine-amadeus-api).
   @override
   Future<String> getRawHotelSearch() async {
     // TODO
@@ -211,6 +287,13 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/destination-content/api-doc/points-of-interest/api-reference
+  /// *"What are the best places to visit in Barcelona?"*
+  ///
+  /// The Points of Interest API finds the best attractions in a given city or area.
+  /// The API provides a ranked list of attractions and the
+  /// name, coordinates, category (sights, beach/park, historical, nightlife, restaurant or shopping),
+  /// tags and score for each one. The scores are powered by the [AVUXI TopPlace](https://www.avuxi.com/)
+  /// algorithm which analyzes millions of online reviews, photos and comments to determine popularity.
   @override
   Future<String> getRawPointsOfInterest() async {
     // TODO
@@ -223,6 +306,15 @@ class AmadeusRemoteDataProvider implements AmadeusBaseDataProvider {
   }
 
   // https://developers.amadeus.com/self-service/category/destination-content/api-doc/safe-place-api/api-reference
+  /// *"How safe is this location?"*
+  ///
+  /// The Safe Place API provides updated safety and security ratings for over 40,000 cities and
+  /// neighborhoods worldwide, helping travelers consult and compare destination safety.
+  /// The API returns an overall safety score and scores for six categories:
+  /// women's safety, health, physical threats, theft, political freedom and LGBTQ safety.
+  /// The scores are powered by the GeoSure GeoSafeScores](https://geosureglobal.com/)
+  /// algorithm which analyzes crime, health and economic data, official travel alerts,
+  /// local reporting and a variety of other sources.
   @override
   Future<String> getRawSafePlace() async {
     // TODO
