8b9949c28c2da7e3f0b238aff9a510cdec31d958
mzdm <zidek.matt@gmail.com>
2020-10-01 18:22:36 +0200

    Add WaveClipper widget

---
+++ lib/ui/pages/home_page/local_widgets/wave_clipper.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+// Source: https://github.com/lohanidamodar/flutter_custom_clippers/blob/master/lib/src/wave_clipper_1.dart
+/// Clip widget in wave shape shape
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size screenSize) {
+    final size = Size(screenSize.width, screenSize.height * .45);
+
+    final firstEndPoint = Offset(size.width * .5, size.height - 20);
+    final firstControlPoint = Offset(size.width * .25, size.height - 45);
+    final secondEndPoint = Offset(size.width, size.height - 50);
+    final secondControlPoint = Offset(size.width * .75, size.height);
+
+    final path = Path()
+      ..lineTo(0.0, size.height)
+      ..quadraticBezierTo(
+        firstControlPoint.dx,
+        firstControlPoint.dy,
+        firstEndPoint.dx,
+        firstEndPoint.dy,
+      )
+      ..quadraticBezierTo(
+        secondControlPoint.dx,
+        secondControlPoint.dy,
+        secondEndPoint.dx,
+        secondEndPoint.dy,
+      )
+      ..lineTo(size.width, 0.0)
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
