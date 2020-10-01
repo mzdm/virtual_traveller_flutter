e3a54b7cdb64b9fd8f8937a94ec33bc4e078eda1
mzdm <zidek.matt@gmail.com>
2020-10-01 18:23:36 +0200

    HomePage styling

--- lib/ui/pages/home_page/home_page.dart
+++ lib/ui/pages/home_page/home_page.dart
@@ -1,13 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_traveller_flutter/ui/pages/home_page/local_widgets/wave_clipper.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(runtimeType.toString()),
+      body: SafeArea(
+        child: ClipPath(
+          clipper: WaveClipper(),
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 4.0),
+                  child: ListTile(
+                    title: Row(
+                      children: <Widget>[
+                        Icon(Icons.place, color: Colors.white, size: 16.0),
+                        SizedBox(width: 12.0),
+                        Text(
+                          'Boston (BOS)',
+                          style: TextStyle(color: Colors.white, fontSize: 14.0),
+                        ),
+                      ],
+                    ),
+                    trailing: InkWell(
+                      onTap: () {},
+                      child: IconButton(
+                        icon: Icon(Icons.settings, color: Colors.white, size: 16.0),
+                        onPressed: () {
+                          print('cliucked');
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
 }
