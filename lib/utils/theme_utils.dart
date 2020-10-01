1db4eb2278080539032c9e824769704b6adcb68b
mzdm <zidek.matt@gmail.com>
2020-10-01 18:22:50 +0200

    Add ThemeUtils

---
+++ lib/utils/theme_utils.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ThemeUtils {
+  // Colors
+  static const primaryDefaultColorBlue = Color(0xFF0064D4);
+
+  // Themes
+  static final defaultDarkBlueTheme = ThemeData(
+    primaryColor: primaryDefaultColorBlue,
+  );
+}
