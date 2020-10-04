8af549783d4f7ecfd91dd76a8cf5c7436d84deea
mzdm <zidek.matt@gmail.com>
2020-10-04 17:04:06 +0200

    Settings page styling

--- lib/presentation/pages/settings_page/settings_page.dart
+++ lib/presentation/pages/settings_page/settings_page.dart
@@ -1,13 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(runtimeType.toString()),
+        title: Text('Settings'),
       ),
-      body: Container(),
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: 'Common',
+            tiles: [
+              SettingsTile(
+                leading: Icon(Icons.language),
+                title: 'Language',
+                subtitle: 'English',
+                onTap: () {},
+              ),
+              SettingsTile(
+                leading: Icon(Icons.monetization_on),
+                title: 'Currency',
+                subtitle: 'USD',
+                onTap: () {},
+              ),
+              SettingsTile(
+                leading: Icon(Icons.thermostat_outlined),
+                title: 'Temperature',
+                subtitle: 'Celsius',
+                onTap: () {},
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Interface',
+            tiles: [
+              SettingsTile(
+                leading: Icon(Icons.brightness_6),
+                title: 'Theme',
+                subtitle: 'Dark blue',
+                onTap: () {},
+              ),
+              SettingsTile.switchTile(
+                leading: Icon(Icons.flight_takeoff),
+                title: 'Virtual mode',
+                subtitle: 'Show immediate travel button',
+                switchValue: true,
+                onToggle: (bool value) {},
+              ),
+            ],
+          ),
+          SettingsSection(
+            title: 'Misc',
+            tiles: [
+              SettingsTile(
+                leading: Icon(Icons.delete),
+                title: 'Remove locally saved data',
+                subtitle: 'Search history, set default settings ...',
+                onTap: () {},
+              ),
+              SettingsTile(
+                leading: Icon(Icons.privacy_tip),
+                title: 'Licenses',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
