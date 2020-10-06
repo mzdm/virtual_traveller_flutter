c96f5b9e193cad83b52d7de62813d83469aedda3
Matej Zidek <zidek.matt@gmail.com>
2020-10-06 08:11:23 +0200

    Search Flights styling

--- lib/presentation/pages/flights_page/search_flights_page.dart
+++ lib/presentation/pages/flights_page/search_flights_page.dart
@@ -5,11 +5,47 @@ class SearchFlightsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(runtimeType.toString()),
+        title: Text('Search Flights'),
       ),
-      body: Column(
-        children: [
-        ],
+      backgroundColor: Colors.grey[200],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Card(
+          margin: EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(0.0),
+          ),
+          color: Colors.white,
+          child: Column(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.85,
+                height: 45.0,
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Search Flights',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.5,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  color: Theme.of(context).primaryColor,
+                  elevation: 0.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.lerp(
+                      BorderRadius.circular(15.0),
+                      BorderRadius.circular(20.0),
+                      0.35,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
