e0889143c61c8b211236cf865bfc853ab7a5fc2b
Matej Zidek <zidek.matt@gmail.com>
2020-10-15 22:22:45 +0200

    Edit Destination model

--- test/data/models/destination_test.dart
+++ test/data/models/destination_test.dart
@@ -5,30 +5,70 @@ import 'package:virtual_traveller_flutter/data/data_providers/remote/amadeus_api
 import 'package:virtual_traveller_flutter/data/models/destination.dart';
 
 void main() {
-  test('destinationTest', () async {
-    final amadeusBaseDataProvider = AmadeusMockedDataProvider();
-    final rawData = await amadeusBaseDataProvider.getRawFlightMostTravelled(null);
-    final data = json.decode(rawData)['data'];
+  group('Destination model', () {
+    test('Flights - Most travelled', () async {
+      final amadeusBaseDataProvider = AmadeusMockedDataProvider();
+      final rawData = await amadeusBaseDataProvider.getRawFlightMostTravelled(null);
+      final data = json.decode(rawData)['data'];
 
-    final destinations = (data as List).map((item) {
-      return Destination.fromJson(item);
-    }).toList();
+      final destinations = (data as List).map((item) {
+        return DestinationBase.fromJson(item);
+      }).toList();
 
-    print(destinations);
-    expect(
-      destinations,
-      [
-        Destination('PAR'),
-        Destination('BCN'),
-        Destination('TCI'),
-        Destination('LON'),
-        Destination('BRU'),
-        Destination('NYC'),
-        Destination('UIO'),
-        Destination('SDQ'),
-        Destination('OPO'),
-        Destination('DXB'),
-      ],
-    );
+      print(destinations);
+
+      expect(
+        destinations,
+        [
+          Destination.base('PAR'),
+          Destination.base('BCN'),
+          Destination.base('TCI'),
+          Destination.base('LON'),
+          Destination.base('BRU'),
+          Destination.base('NYC'),
+          Destination.base('UIO'),
+          Destination.base('SDQ'),
+          Destination.base('OPO'),
+          Destination.base('DXB'),
+        ],
+      );
+
+      expect(
+        destinations,
+        [
+          DestinationBase('PAR'),
+          DestinationBase('BCN'),
+          DestinationBase('TCI'),
+          DestinationBase('LON'),
+          DestinationBase('BRU'),
+          DestinationBase('NYC'),
+          DestinationBase('UIO'),
+          DestinationBase('SDQ'),
+          DestinationBase('OPO'),
+          DestinationBase('DXB'),
+        ],
+      );
+    });
+
+    test('Flights - Travel recommendation', () async {
+      final rawData = await AmadeusMockedDataProvider().getRawTravelRecommendation(null);
+      final data = json.decode(rawData)['data'];
+
+      final destinations = (data as List).map((item) {
+        return DestinationIata.fromJson(item);
+      }).toList();
+
+      print(destinations);
+
+      expect(destinations, [
+        Destination.iata('PAR'),
+        Destination.iata('MAD'),
+      ]);
+
+      expect(destinations, [
+        DestinationIata('PAR'),
+        DestinationIata('MAD'),
+      ]);
+    });
   });
 }
--- test/main_test.dart
+++ test/main_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('passing test', () async {
+  test('passing test', () {
     expect(true, true);
   });
 }
