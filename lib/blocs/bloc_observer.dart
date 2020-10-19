4d0434f5fb88c891cb9882aee5a17b749c5497fb
Matej Zidek <zidek.matt@gmail.com>
2020-10-19 17:22:20 +0200

    Improve logging messages

--- lib/blocs/bloc_observer.dart
+++ lib/blocs/bloc_observer.dart
@@ -3,25 +3,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class SimpleBlocObserver extends BlocObserver {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print('Bloc onEvent:\n$event');
+    print('${bloc.toString()} onEvent:\n$event');
     super.onEvent(bloc, event);
   }
 
   @override
   void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print('Bloc onError:\n$error');
+    print('${cubit.toString()} onError:\n$error');
     super.onError(cubit, error, stackTrace);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print('Bloc onTransition:\n$transition');
+    print('${bloc.toString()} onTransition:\n$transition');
     super.onTransition(bloc, transition);
   }
 
   @override
   void onChange(Cubit cubit, Change change) {
-    print('Bloc onChange:\n$change');
+    print('${cubit.toString()} onChange:\n$change');
     super.onChange(cubit, change);
   }
 }
