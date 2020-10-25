a97be0a4e270aaf9b5a09b3874a4decf93219249
Matej Zidek <zidek.matt@gmail.com>
2020-10-25 12:42:18 +0100

    Rename class

--- lib/presentation/pages/flights/flight_results_page.dart
+++ lib/presentation/pages/flights/flight_results_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 // TODO:
-class FlightsResultPage extends StatelessWidget {
+class FlightResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
