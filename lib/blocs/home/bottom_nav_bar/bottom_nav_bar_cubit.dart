90288f9ac7363666215ef25b584ae8a72b3bc516
Matej Zidek <zidek.matt@gmail.com>
2020-11-08 13:55:30 +0100

    Remove unused imports

--- lib/blocs/home/bottom_nav_bar/bottom_nav_bar_cubit.dart
+++ lib/blocs/home/bottom_nav_bar/bottom_nav_bar_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
 
 class BottomNavBarCubit extends Cubit<int> {
   BottomNavBarCubit() : super(0);
--- lib/presentation/pages/destination/pois_page.dart
+++ lib/presentation/pages/destination/pois_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:virtual_traveller_flutter/blocs/destination/poi/pois_cubit.dart';
